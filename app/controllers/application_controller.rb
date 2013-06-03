0b3a36ff6176ff8771e7ee0e7389ddf1bf837d7c
waynenabors <wayne@aequ.com>
2013-06-03 16:37:22 -0500

    Recover from un authorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => "You do not have access to that page. You can request access by emailing info@servemoore.com"
+  end
+  
 end
