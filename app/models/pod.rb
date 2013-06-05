300f19fe019677857837ab49bd71b60b537f8e23
waynenabors <wayne@aequ.com>
2013-06-05 00:09:23 -0500

    Changes to Pod model

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -1,3 +1,17 @@
 class Pod < ActiveRecord::Base
   attr_accessible :desc, :notice, :properties, :title
+  
+  delegate :latitude, to: :address
+  delegate :longitude, to: :address
+
+  belongs_to :point_of_contact, :polymorphic => true
+  belongs_to :address
+  accepts_nested_attributes_for :address
+
+  acts_as_gmappable :process_geocoding => false
+
+  def gmaps4rails_address
+   "#{self.latitude}, #{self.longitude}"
+  end
+  
 end
