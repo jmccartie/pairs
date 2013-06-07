dfbb2bafce41ab5906d3a4dade5dea4287f136f0
waynenabors <wayne@aequ.com>
2013-06-06 20:47:59 -0500

    Resources Page

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,12 +1,19 @@
 class Good < ActiveRecord::Base
   STATUSES = ['Need More', 'Have Enough', 'Too Much']
+  CATEGORIES = ["Food and Water", "Tools", "Power Equipment", "Clothing", "Household Items", "Personal Care Items", "Other"]
   
   attr_accessible :category, :desc, :drop_off, :pick_up, :pod_id, :status, :title
+  belongs_to :pod
   
   # Validations
   validates_inclusion_of :status, in: STATUSES
+  validates_inclusion_of :category, in: CATEGORIES
   
   def self.statuses
     STATUSES
+  end
+  
+  def self.categories
+    CATEGORIES
   end
 end
